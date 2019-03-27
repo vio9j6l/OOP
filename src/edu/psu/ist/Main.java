e904e38a710c5920e0ebf2db6a5ddf64ce33ce61
joeoakes <joe.oakes@gmail.com>
2019-03-27 19:20:56 -0400

    Initial commit

---
+++ src/edu/psu/ist/Main.java
@@ -0,0 +1,16 @@
+package edu.psu.ist;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Cake cake = new Cake();  //Cannot be instantiated
+
+        AngelFood angelCake = new AngelFood();
+        System.out.println("Angel's Food Ingredients");
+        angelCake.printIngredients();
+
+        DevilFood devilCake = new DevilFood();
+        System.out.println("Devils's Food Ingredients");
+        devilCake.printIngredients();
+    }
+}
